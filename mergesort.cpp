31be41bae118a58f4f5abcd9093058d279c5957c
piyushmittal50 <105166314+Piyushmittal50@users.noreply.github.com>
2022-09-18 15:24:36 +0530

    Create mergesort.cpp

---
+++ mergesort.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+void merge(int arr[],int l,int mid,int r)
+{
+    int n1=mid-l+1;
+    int n2=r-mid;
+
+    //temp variable
+    int a[n1];
+    int b[n2];
+
+    for(int i=0;i<n1;i++)
+    {
+        a[i]=arr[l+i];   
+    }
+     for(int i=0;i<n2;i++)
+     {
+        b[i] =arr[mid+1+i];
+     }
+     int i=0;
+     int j=0;
+     int k=l;
+     while(i<n1&&j<n2)
+     {
+        if(a[i]<b[j])
+        {
+            arr[k]=a[i];
+            k++;
+            i++;
+        }
+        else
+        {
+            arr[k]=a[i];
+            k++;
+            j++;
+        }
+        while(i<n1)
+        {
+            arr[k]=arr[i];
+            k++;
+            i++;
+        }
+        while(i<n2)
+        {
+            arr[k]=arr[j];
+            k++;
+            j++;
+        }
+     }
+}
+void mergesort( int arr[],int l,int r)
+{
+   int mid=(l+r)/2;
+   mergesort(arr,l,mid);
+   mergesort(arr,mid+1,r);
+
+   merge(arr,l,mid,r);
+}
+int main()
+{
+    int arr[]={5,4,3,2,1};
+    mergesort(arr,0,4);
+    for(int i=0;i<5;i++)
+    {
+        cout<<arr[i]<<" ";   
+    }
+    cout<<endl;
+}
